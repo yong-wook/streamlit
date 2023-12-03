90cd1f5112ab94476b7d1118a768bb59f223cc60
yong-wook <91772701+yong-wook@users.noreply.github.com>
2023-12-04 00:06:09 +0900

    Update 3_🌐_크롤러.py

--- pages/3_🌐_크롤러.py
+++ pages/3_🌐_크롤러.py
@@ -2,7 +2,7 @@ import streamlit as st
 import webbrowser
 
 if st.session_state["auth_login"] != "로그인 중":
-    webbrowser.open("http://localhost:8501")
+    webbrowser.open("https://usewookstest.streamlit.app/")
 
 st.header("각종 크롤러 연습용")
 st.sidebar.success(st.session_state["auth_login"])
