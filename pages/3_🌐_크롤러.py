fd231cd53aedb97c11495f41eaa8736ada021d26
yong-wook <91772701+yong-wook@users.noreply.github.com>
2023-12-04 00:24:15 +0900

    Update 3_🌐_크롤러.py

--- pages/3_🌐_크롤러.py
+++ pages/3_🌐_크롤러.py
@@ -5,7 +5,7 @@ if "auth_login" not in st.session_state:
    st.session_state["auth_login"] = None 
 
 if st.session_state["auth_login"] != "로그인 중":
-    webbrowser.open("https://usewookstest.streamlit.app/")
+    webbrowser.get("https://usewookstest.streamlit.app/")
 
 st.header("각종 크롤러 연습용")
 st.sidebar.success(st.session_state["auth_login"])
