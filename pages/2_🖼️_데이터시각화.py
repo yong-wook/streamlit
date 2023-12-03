8fc81dbe7ff583bdbae8096060450ef4119e96df
yong-wook <91772701+yong-wook@users.noreply.github.com>
2023-12-04 00:47:13 +0900

    Update 2_🖼️_데이터시각화.py

--- pages/2_🖼️_데이터시각화.py
+++ pages/2_🖼️_데이터시각화.py
@@ -1,11 +1,12 @@
 import streamlit as st
 import webbrowser
+from streamlit_extras.switch_page_button import switch_page
 
 if "auth_login" not in st.session_state:
    st.session_state["auth_login"] = None 
 
 if st.session_state["auth_login"] != "로그인 중":
-    webbrowser.get("https://usewookstest.streamlit.app/")
+    switch_page("home")
     
 
 st.header("데이터 시각화 연습용")
