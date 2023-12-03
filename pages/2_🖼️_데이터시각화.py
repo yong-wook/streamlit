e70bce62cd830b442e913511234af9754edbddba
yong-wook <91772701+yong-wook@users.noreply.github.com>
2023-12-04 00:05:54 +0900

    Update 2_🖼️_데이터시각화.py

--- pages/2_🖼️_데이터시각화.py
+++ pages/2_🖼️_데이터시각화.py
@@ -3,7 +3,7 @@ import webbrowser
 
 
 if st.session_state["auth_login"] != "로그인 중":
-    webbrowser.open("http://localhost:8501")
+    webbrowser.open("https://usewookstest.streamlit.app/")
     
 
 st.header("데이터 시각화 연습용")
